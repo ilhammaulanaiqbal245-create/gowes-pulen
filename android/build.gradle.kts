f014dfa8a94ae510eea75b41937b47d380e13894
root <root@localhost.localdomain>
2026-08-01 04:54:37 +0700

    Final Sweep: Clean build.gradle.kts Kotlin DSL & lock minSdk to 21

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.android.build.gradle.BaseExtension
+
 allprojects {
     repositories {
         google()
@@ -5,31 +7,14 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
-
-
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
-}
-
-
-        }
-    }
-}
 subprojects {
-    plugins.withId("com.android.library") {
-        extensions.configure<com.android.build.gradle.BaseExtension> {
+    project.pluginManager.withPlugin("com.android.library") {
+        project.extensions.configure<BaseExtension> {
             compileSdkVersion(34)
         }
     }
-    plugins.withId("com.android.application") {
-        extensions.configure<com.android.build.gradle.BaseExtension> {
+    project.pluginManager.withPlugin("com.android.application") {
+        project.extensions.configure<BaseExtension> {
             compileSdkVersion(34)
         }
     }
