a55b6f78ffd0de4de83b21eee4c8a5b2091eec57
root <root@localhost.localdomain>
2026-08-01 03:11:14 +0700

    Regenerate clean android project for v2 embedding

---
+++ android/app/src/main/kotlin/com/example/gowes_pulen/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.gowes_pulen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/ilgammaulaniaiqbal245/gowes_pulen/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.ilgammaulaniaiqbal245.gowes_pulen
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
