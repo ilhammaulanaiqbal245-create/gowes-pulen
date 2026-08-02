a97014cf4fef92d9cd3cbc13029ea939a124f468
ilhammaulanaiqbal245-create <ilhammaulanaiqbal245@gmail.com>
2026-08-02 07:58:25 +0700

    Fix app build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application")
-    id("kotlin-android")
+    id("org.jetbrains.kotlin.android")
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -8,11 +8,6 @@ android {
     namespace = "com.example.gowes_pulen"
     compileSdk = 36
     ndkVersion = flutter.ndkVersion
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
 
     defaultConfig {
         applicationId = "com.example.gowes_pulen"
@@ -22,8 +17,13 @@ android {
         versionName = flutter.versionName
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
     buildTypes {
-        getByName("release") {
+        release {
             signingConfig = signingConfigs.getByName("debug")
             isMinifyEnabled = false
             isShrinkResources = false
@@ -38,5 +38,5 @@ kotlin {
 }
 
 flutter {
-    source = "../../"
+    source = "../.."
 }
